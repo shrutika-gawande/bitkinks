ddea2cf8be9ad837e3bec336e7fe66099bd331f9
shrutika-gawande <shrutikagawande80@gmail.com>
2025-05-20 22:27:11 +0530

    updated

--- bitlinks/app/redirect/[shortId]/page.js
+++ bitlinks/app/redirect/[shortId]/page.js
@@ -5,7 +5,7 @@ export async function GET(request, { params }) {
 
   try {
     const client = await clientPromise;
-    const db = client.db('bitlinks');
+    const db = client.db('BitLinks');
     const collection = db.collection('urls');
 
     const result = await collection.findOne({ shortId });
@@ -21,3 +21,4 @@ export async function GET(request, { params }) {
     return new Response('Internal Server Error', { status: 500 });
   }
 }
+//err
